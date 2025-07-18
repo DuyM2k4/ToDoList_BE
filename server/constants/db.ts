ca3b1ca7131c284dd0e35bb0cf21d7175c11dfbd
Phạm Nguyễn Mạnh Duy <duym2k4@gmail.com>
2025-07-19 00:36:14 +0700

    Xóa toàn bộ code ở nhánh main

--- server/constants/db.ts
+++
@@ -1,4 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config(); 
-
-export const MONGO_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`;
