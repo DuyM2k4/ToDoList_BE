d15c63381c8b92baa5990cc6c74c2f702008a3c0
Phạm Nguyễn Mạnh Duy <duym2k4@gmail.com>
2025-07-19 01:27:03 +0700

    Update MongoDB URI to use 'todolist' database

--- server/constants/db.ts
+++ server/constants/db.ts
@@ -1,4 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config(); 
 
-export const MONGO_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`;
+export const MONGO_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/todolist?retryWrites=true&w=majority&appName=${process.env.DB_APPNAME}`;
