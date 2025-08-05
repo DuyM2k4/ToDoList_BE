a6c5c4419e1252117760fa0e02fe15b364bac20e
haha <giang1462004@gmail.com>
2025-08-05 18:13:39 +0000

    haha - add comment

--- server/utils/logger.ts
+++ server/utils/logger.ts
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 import winston from "winston";
 import DailyRotateFile from "winston-daily-rotate-file";
 
+// Set level
 const LogLevels = {
   error: 0,
   warn: 1,
@@ -10,135 +11,42 @@ const LogLevels = {
   debug: 4
 };
 
+// Set environment 
 const ENV_LOG_LEVEL = 'debug'
 const LogLevel = ENV_LOG_LEVEL || "info"
+// const LogLevel = process.env.LOG_LEVEL || 'info';
 
+// Set format to print
 export const customLogFormat = winston.format.printf(
-      ({ timestamp, level, message, logMetadata, stack }) => {
-        return `${timestamp} ${level.toUpperCase()}: ${logMetadata || ''} ${message} ${stack || ""}`;
+      ({ timestamp, level, endpoint, message, logMetadata, stack }) => {
+        return `${timestamp} ${level.toUpperCase()} ${endpoint || ''}: ${logMetadata || ''} ${message} ${stack || ""}`;
       });
 
-const logger = winston.createLogger({
-  levels: LogLevels,
-  level: LogLevel,
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }),
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss:SSS",
-    }),
-    customLogFormat
-  ),
-  transports: [new winston.transports.Console()]
-})
+// Base format
+const baseFormat = winston.format.combine(
+  winston.format.errors({ stack: true }),
+    // winston.format.timestamp(),
+    // winston.format.json()
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:SSS" }),
+  customLogFormat
+);
 
-
+// Set file rotate
 const fileRotateTransport = new DailyRotateFile({
   filename: "server/log/application-%DATE%.log",
   datePattern: "YYYY-MM-DD",
   zippedArchive: true,
   maxSize: "20m",
   maxFiles: "14d",
-  format: winston.format.combine(
-    winston.format.errors({ stack: true }),
-    // winston.format.timestamp(),
-    // winston.format.json()
-    winston.format.timestamp({
-      format: "YYYY-MM-DD HH:mm:ss:SSS"
-    }),
-    customLogFormat
-  ),
+  format: baseFormat
 });
-
-logger.add(fileRotateTransport);
+    
+// Create logger
+const logger = winston.createLogger({
+  levels: LogLevels,
+  level: LogLevel,
+  format: baseFormat,
+  transports: [new winston.transports.Console(), fileRotateTransport]
+})
 
 export default logger;
-
-logger.info("haha")
-logger.error(new Error("hehe"))
-logger.warn("kkk")
-logger.warn("Thiếu thông tin user", {
-  userId: 123,
-  endpoint: "/api/login"
-});
-logger.child({
-  logMetadata1: `User: 123`
-})
-  .debug("is requesting task!", { user: 123, logMetadata: `User: 123` })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//--------------------------------------------------------------------------------------
-// import fs from 'fs';
-// import path from 'path';
-
-// // Đường dẫn tới thư mục log và file log
-// const logDir = path.join(__dirname, '../log'); // Thư mục lưu log
-// const logFile = path.join(logDir, 'server.log'); // File log chính
-
-// // Đảm bảo thư mục log tồn tại, nếu chưa có thì tạo mới
-// if (!fs.existsSync(logDir)) {
-//   fs.mkdirSync(logDir);
-// }
-
-// // Hàm ghi log vào file với định dạng thời gian, mức log và nội dung
-// function writeLog(level: string, message: string) {
-//   const time = new Date().toISOString(); // Lấy thời gian hiện tại
-//   const logMsg = `[${time}] [${level}] ${message}\n`;
-//   fs.appendFileSync(logFile, logMsg, { encoding: 'utf8' }); // Ghi vào file log
-// }
-
-// // Đối tượng logger hỗ trợ log ra console và file với 3 mức: info, warn, error
-// export const logger = {
-//   info: (msg: string) => {
-//     console.log(msg); // In ra console
-//     writeLog('INFO', msg); // Ghi vào file log
-//   },
-//   warn: (msg: string) => {
-//     console.warn(msg); // In ra console với màu vàng
-//     writeLog('WARN', msg); // Ghi vào file log
-//   },
-//   error: (msg: string) => {
-//     console.error(msg); // In ra console với màu đỏ
-//     writeLog('ERROR', msg); // Ghi vào file log
-//   }
-// }; 
